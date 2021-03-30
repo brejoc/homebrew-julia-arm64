5b9ac8e6dd81c9bbc3a72a38185d7a839659fa23
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-03-30 13:34:22 -0400

    Update julia-master.rb

--- Formula/julia-master.rb
+++ Formula/julia-master.rb
@@ -1,9 +1,8 @@
 class JuliaMaster < Formula
-  desc "The Julia Programming Language, installed from the master branch on GitHub."
+  desc "Julia Programming Language, installed from the master branch on GitHub"
   homepage "https://github.com/JuliaLang/julia"
-  url "https://github.com/JuliaLang/julia/archive/refs/heads/master.zip"
+  url "https://github.com/JuliaLang/julia/archive/refs/heads/master.tar.gz"
   version "1.7"
-  sha256 "" 
   license "MIT"
 
   depends_on "make" => :build
@@ -16,15 +15,6 @@ class JuliaMaster < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test julia-nightly-source`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/julia", "--version"
   end
 end
