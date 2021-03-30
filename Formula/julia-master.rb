cd4632f66f9dabc87fb2aaf2ac2192a255407ada
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-03-30 13:41:18 -0400

    Update julia-master.rb

--- Formula/julia-master.rb
+++ Formula/julia-master.rb
@@ -6,7 +6,7 @@ class JuliaMaster < Formula
   license "MIT"
 
   depends_on "make" => :build
-  conflicts_with cask: "julia", "julia-nightly"
+  conflicts_with cask: 'local-caffeine'
 
   def install
     system "echo 'Building Julia from source. This will take a while!'"
