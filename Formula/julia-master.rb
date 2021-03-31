76fdfe98f051f890990164b1303722cff8807753
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-03-31 10:01:08 -0400

    Update julia-master.rb

--- Formula/julia-master.rb
+++ Formula/julia-master.rb
@@ -13,7 +13,7 @@ class JuliaMaster < Formula
   def install
     system "echo 'Building Julia from source. This will take a while!'"
     system "ENV[prefix]=#{prefix}"
-    system "make -j $(python -c 'import multiprocessing as mp; print(mp.cpu_count()+1)')", "install"
+    system "make", "-j $(python -c 'import multiprocessing as mp; print(mp.cpu_count()+1)')", "install"
   end
 
   test do
