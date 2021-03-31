e8183d75c47e6aaf39fa71d54b45a58a4712ad4a
Joe Carpinelli <jdcarpinelli@gmail.com>
2021-03-31 14:37:16 -0400

    Update julia-master.rb

--- Formula/julia-master.rb
+++ Formula/julia-master.rb
@@ -13,7 +13,7 @@ class JuliaMaster < Formula
   def install
     system "echo 'Building Julia from source. This will take a while!'"
     system "ENV[prefix]=#{prefix}"
-    system "make -j9" "install"
+    system "make" "install"
   end
 
   test do
