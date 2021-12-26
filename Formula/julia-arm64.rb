b29c789a98874fd28edd905566fa676b53f9a50e
Jochen Breuer <brejoc@gmail.com>
2021-12-26 11:58:35 +0100

    Fix for class name

--- Formula/julia-arm64.rb
+++ Formula/julia-arm64.rb
@@ -1,4 +1,4 @@
-class JuliaMaster < Formula
+class JuliaArm64 < Formula
   desc "Julia Programming Language, installed from the master branch on GitHub"
   homepage "https://github.com/JuliaLang/julia"
   url "https://github.com/JuliaLang/julia/releases/download/v1.7.1/julia-1.7.1-full.tar.gz"
